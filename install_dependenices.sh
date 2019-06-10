2fa1b4c643fb35261986d277c21d5c3122bf2242
gaunthan <gaunthanhuang@foxmail.com>
2019-06-10 09:42:59 +0800

    Add missing dependency

--- install_dependenices.sh
+++ install_dependenices.sh
@@ -8,7 +8,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j2
 
 # install isam dependency
-sudo apt-get install cmake libsuitesparse-dev libeigen3-dev libsdl1.2-dev doxygen graphviz ros-*-pcl-conversions
+sudo apt-get install cmake libsuitesparse-dev libeigen3-dev libsdl1.2-dev doxygen graphviz ros-*-pcl-conversions ross-*-pcl-ros
 
 # install python library
 sudo easy_install pip
